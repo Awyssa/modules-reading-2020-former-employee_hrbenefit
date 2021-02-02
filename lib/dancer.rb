19a4b8c1a68b7c55634617120c2d8e37dc121d29
Michael Ernest <m.hendersun@gmail.com>
2021-02-02 09:33:38 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -8,26 +8,26 @@ class Kid
   def initialize(name)
     @name = name
   end
-  
+
 end
 
 # class Kid
 #   include Dance
-# 
+#
 #   attr_accessor :name
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
 # end
-# 
-# 
-# 
+#
+#
+#
 # class Dancer
 #   include Dance
-# 
+#
 #   attr_accessor :name
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   end
