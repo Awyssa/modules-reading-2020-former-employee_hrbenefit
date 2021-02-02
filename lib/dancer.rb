d02fbb0a8dca5aed86adcf988418e662a895b8b1
Michael Ernest <m.hendersun@gmail.com>
2021-02-02 09:33:35 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,22 +3,32 @@ require_relative './dance_module.rb'
 
 class Kid
   include Dance
-
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
+  
 end
 
-
-
-class Dancer
-  include Dance
-
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
+# class Kid
+#   include Dance
+# 
+#   attr_accessor :name
+# 
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+# 
+# 
+# 
+# class Dancer
+#   include Dance
+# 
+#   attr_accessor :name
+# 
+#   def initialize(name)
+#     @name = name
+#   end
+# end
